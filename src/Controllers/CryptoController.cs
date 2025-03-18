f1fd85f468065193a22d496b211c16cb2acd31ab
joaostack <>
2025-03-18 18:01:45 -0300

    GetCoin function

--- src/Controllers/CryptoController.cs
+++ src/Controllers/CryptoController.cs
@@ -23,6 +23,13 @@ namespace API1.Controllers
             return Ok(obj);
         }
 
+        [HttpGet("coin/{name}")]
+        public IActionResult GetCoinAll(string name)
+        {
+            var coin = GetCoin(name);
+            return Ok(coin);
+        }
+
         // WebScraping starts here...
         public static List<dynamic> GetAllCoins(string page)
         {
@@ -60,6 +67,30 @@ namespace API1.Controllers
             else
             {
                 return new List<object>() { new { Message = "Error on scraping, no data found!" } };
+            }
+        }
+
+        public static object GetCoin(string name)
+        {
+            if (name != null)
+            {
+                name = name.Trim().ToLower();
+
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument htmlDocument = web.Load($"https://coinmarketcap.com/currencies/{name}");
+                var price = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='section-coin-overview']//span");
+                if (price != null)
+                {
+                    return new { Name = name, Price = price.InnerText.Trim() };
+                }
+                else
+                {
+                    return "Price not found!";
+                }
+            }
+            else
+            {
+                return "Coin not found!";
             }
         }
     }
